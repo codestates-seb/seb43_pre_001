4f7d5fc6f5d4f1fa23e1d8c1c9b6786722f12245
seunghyun711 <seunghyun711@gmail.com>
2023-04-17 11:16:39 +0900

    refactor:MemberDto,Post 수정

--- server/src/main/java/com/preproject/server/member/MemberDto.java
+++ server/src/main/java/com/preproject/server/member/MemberDto.java
@@ -15,9 +15,6 @@ public class MemberDto {
     @NoArgsConstructor
     @Getter
     public static class Post{
-        @Positive
-        private long memberId;
-
         @Valid
         @NotNull(message = "닉네임을 입력하세요.")
         private String nickname;
@@ -25,6 +22,10 @@ public class MemberDto {
         @Valid
         @NotNull(message = "이름을 입력하세요.")
         private String name;
+
+        @Valid
+        @NotNull(message = "이메일을 입력하세요.")
+        private String email;
 
         @Valid
         @NotNull(message = "패스워드를 입력하세요.")
