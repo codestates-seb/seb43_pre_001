90e109e8130611be8ae719ca444d01e0b32e4b89
seunghyun711 <seunghyun711@gmail.com>
2023-04-17 23:08:47 +0900

    refactor: 사용자 권한 정보 테이블과 매핑

--- server/src/main/java/com/preproject/server/member/entity/Member.java
+++ server/src/main/java/com/preproject/server/member/entity/Member.java
@@ -5,12 +5,15 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @NoArgsConstructor
 @Getter
 @Setter
-public class Member {
+public class Member implements Principal {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long memberId;
@@ -26,4 +29,8 @@ public class Member {
 
     @Column(nullable = false)
     private String password;
+
+    // 사용자 권한 정보 테이블과 매핑
+    @ElementCollection(fetch = FetchType.EAGER)
+    private List<String> roles = new ArrayList<>();
 }
