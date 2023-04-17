abc16a4f294705a73c42a2027b5ac2037683e622
HJKKIM <hoj3646@naver.com>
2023-04-17 16:46:31 +0900

    feat: AnswerController - 답변 작성 구현

---
+++ server/src/main/java/com/preproject/server/answer/controller/AnswerController.java
@@ -0,0 +1,38 @@
+package com.preproject.server.answer.controller;
+
+import com.preproject.server.answer.dto.AnswerPatchDto;
+import com.preproject.server.answer.dto.AnswerPostDto;
+import com.preproject.server.answer.entity.Answer;
+import com.preproject.server.answer.mapper.AnswerMapper;
+import com.preproject.server.answer.service.AnswerService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@RestController
+@Validated
+@RequestMapping("/answers")
+public class AnswerController {
+    private AnswerService answerService;
+    private AnswerMapper mapper;
+
+    public AnswerController(AnswerService answerService, AnswerMapper mapper){
+        this.answerService = answerService;
+        this.mapper = mapper;
+    }
+
+    // 답변 작성
+    @PostMapping
+    public ResponseEntity postAnswer(@Valid @RequestBody AnswerPostDto answerPostDto){
+        Answer question = answerService.createAnswer(
+                mapper.answerPostDtoToAnswer(answerPostDto));
+
+        return new ResponseEntity<>(mapper.answerToAnswerResponseDto(question), HttpStatus.CREATED);
+    }
+
+}
