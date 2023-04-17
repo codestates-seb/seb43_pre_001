218199667d5274066266d1b188d13842298d857f
HJKKIM <hoj3646@naver.com>
2023-04-17 11:42:28 +0900

    feat: AnswerResponseDto 구현

---
+++ server/src/main/java/com/preproject/server/answer/dto/AnswerResponseDto.java
@@ -0,0 +1,11 @@
+package dto;
+
+import lombok.Data;
+
+public class AnswerResponseDto {
+    private Long answerId;
+    private String member;
+    private String content;
+    private Data createdAt;
+
+}
