5e4ca69571d346037edca643f685e59326d6be31
HJKKIM <hoj3646@naver.com>
2023-04-17 11:21:21 +0900

    feat: AnswerCreateDto 구현

---
+++ server/src/main/java/com/preproject/server/answer/dto/AnswerCreateDto.java
@@ -0,0 +1,21 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerCreateDto {
+    @NotNull
+    private String member;
+
+    @NotEmpty
+    private String content;
+}
