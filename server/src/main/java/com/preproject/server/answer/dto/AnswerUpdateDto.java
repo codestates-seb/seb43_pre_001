c96a0b3b037c05ac4ba9599df3cb03ac02ce6d2f
HJKKIM <hoj3646@naver.com>
2023-04-17 11:31:04 +0900

    feat: AnswerUpdateDto 구현

---
+++ server/src/main/java/com/preproject/server/answer/dto/AnswerUpdateDto.java
@@ -0,0 +1,20 @@
+package dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AnswerUpdateDto {
+    @NotNull
+    private String member;
+    @NotEmpty
+    private String content;
+}
