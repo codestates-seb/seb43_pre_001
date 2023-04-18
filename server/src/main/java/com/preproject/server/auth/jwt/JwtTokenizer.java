96272dc9a79bdc76faaaa013071c6c003ef213c6
seunghyun711 <seunghyun711@gmail.com>
2023-04-18 11:07:27 +0900

    feat: JWT 만료일시 설정

--- server/src/main/java/com/preproject/server/auth/jwt/JwtTokenizer.java
+++ server/src/main/java/com/preproject/server/auth/jwt/JwtTokenizer.java
@@ -1,9 +1,14 @@
 package com.preproject.server.auth.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.io.Encoders;
 import io.jsonwebtoken.security.Keys;
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
@@ -11,7 +16,19 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.Map;
 
+@Component
 public class JwtTokenizer {
+    @Getter
+    @Value("${jwt.key}")
+    private String secretKey; // JWT 생성 & 검증에 사용되는 Secret Key
+
+    @Getter
+    @Value("${jwt.access-token-expiration-minutes}")
+    private String accessTokenExpirationMinutes; // AccessToken 만료 시간
+
+    @Getter
+    @Value("${jwt.refresh-token-expiration-minutes}")
+    private String refreshTokenExpirationMinutes; // RefreshToken 만료 시간
 
     // Plain text 형태의 secretKey를 Base64 형식의 문자열로 인코딩
     public String encodedBase64SecretKey(String secretKey) {
@@ -56,6 +73,17 @@ public class JwtTokenizer {
         return key;
     }
 
+    public Jws<Claims> getClaims(String jws, String base64EncodedSecretKey) {
+        Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
+
+        Jws<Claims> claims = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(jws);
+
+        return claims;
+    }
+
     // JWT 검증 메서드
     public void verifySignature(String jws, String base64EncodedSecretKey) {
         Key key = getKeyFromBase64EncodedKey(base64EncodedSecretKey);
@@ -64,5 +92,14 @@ public class JwtTokenizer {
                 .setSigningKey(key) // 서명에 사용된 SecretKey 설정
                 .build()
                 .parseClaimsJws(jws);
+    }
+
+    // JWT 만료 일시 설정 메서드
+    public Date getTokenExpiration(int expirationMinutes) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(Calendar.MINUTE, expirationMinutes);
+        Date expiration = calendar.getTime();
+
+        return expiration;
     }
 }
