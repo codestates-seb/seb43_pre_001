e96f95b4e455c845c97e13959cfdc998891bf9b1
seunghyun711 <seunghyun711@gmail.com>
2023-04-18 13:04:23 +0900

    feat: CORS 정책 설정 구현

--- server/src/main/java/com/preproject/server/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/preproject/server/auth/config/SecurityConfiguration.java
@@ -3,10 +3,18 @@ package com.preproject.server.auth.config;
 import com.preproject.server.auth.utils.MemberAuthorityUtils;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.*;
 
 @Configuration
 public class SecurityConfiguration {
@@ -22,6 +30,7 @@ public class SecurityConfiguration {
                 .headers().frameOptions().sameOrigin()
                 .and()
                 .csrf().disable()
+                .cors(withDefaults()) // corsConfigurationSource라는 이름의 Bean 사용
                 .formLogin().disable()
                 .httpBasic().disable()
                 .authorizeHttpRequests(authorize -> authorize
@@ -35,4 +44,16 @@ public class SecurityConfiguration {
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
+    // CORS 정책 설정
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*")); // 모든 출처에 대해 HTTP 통신 허용
+        configuration.setAllowedMethods(Arrays.asList("GET","POST","PATCH","DELETE")); // 해당 HTTP 메서드에 대한 통신 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // 모든 URL에 CORS 정책 적용
+
+        return source;
+    }
 }
