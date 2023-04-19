0c28201ea4a1b82706d549c880fbed13e38291a5
seunghyun711 <seunghyun711@gmail.com>
2023-04-20 02:58:48 +0900

    feat: MemberAuthenticationEntryPoint 설정 추가

--- server/src/main/java/com/preproject/server/auth/config/SecurityConfiguration.java
+++ server/src/main/java/com/preproject/server/auth/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.preproject.server.auth.config;
 
 import com.preproject.server.auth.filter.JwtAuthenticationFilter;
 import com.preproject.server.auth.filter.JwtVerificationFilter;
+import com.preproject.server.auth.handler.MemberAuthenticationEntryPoint;
 import com.preproject.server.auth.handler.MemberAuthenticationFailureHandler;
 import com.preproject.server.auth.handler.MemberAuthenticationSuccessHandler;
 import com.preproject.server.auth.jwt.JwtTokenizer;
@@ -45,6 +46,9 @@ public class SecurityConfiguration {
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
+                .exceptionHandling()
+                .authenticationEntryPoint(new MemberAuthenticationEntryPoint()) // MemberAuthenticationEntryPoint 추가
+                .and()
                 .apply(new CustomFilterConfigurer()) // CustomFilterConfigurer() 추가
                 .and()
                 .authorizeHttpRequests(authorize -> authorize
