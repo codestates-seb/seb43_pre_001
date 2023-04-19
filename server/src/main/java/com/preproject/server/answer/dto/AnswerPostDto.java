2a41a1ea2a8aee5624bd84a5292e69d33481d145
HJKKIM <hoj3646@naver.com>
2023-04-19 17:57:39 +0900

    refactor: 유효성 검증 추가

--- server/src/main/java/com/preproject/server/answer/dto/AnswerPostDto.java
+++ server/src/main/java/com/preproject/server/answer/dto/AnswerPostDto.java
@@ -2,20 +2,21 @@ package com.preproject.server.answer.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class AnswerPostDto { // Request Body
     @NotNull
+    @Positive
     private Long question_id;
     @NotNull
     private Long member_id;
-    @NotEmpty
+    @NotBlank(message = "답변 내용을 적어주세요")
     private String content;
 }
