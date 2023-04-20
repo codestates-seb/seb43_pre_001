313698deb0007fb92361985b2a369085cf04cf61
Choi-hyeona <xhrl523@naver.com>
2023-04-20 19:24:46 +0900

    feat: header를  로그인 여부에 따라 처리함

--- client/src/pages/QuestionsPage.jsx
+++ client/src/pages/QuestionsPage.jsx
@@ -1,10 +1,14 @@
 import HeadNotLoginMain from '../components/StackHead/HeadNotLoginMain';
+import HeadLogin from '../components/StackHead/HeadLogin';
 import Questions from '../components/StackQuestions/Questions';
+import { useSelector } from 'react-redux';
 
 const QuestionsPage = ({ curTab, onTabSelect }) => {
+  const { loggedIn } = useSelector((state) => state.user);
+
   return (
     <>
-      <HeadNotLoginMain />
+      {loggedIn ? <HeadLogin /> : <HeadNotLoginMain />}
       <Questions curTab={curTab} onTabSelect={onTabSelect} />
     </>
   );
