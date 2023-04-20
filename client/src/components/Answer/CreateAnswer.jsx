3ebf4c3ecba7f8047185b2423b67f8219c5b98f0
leekoby <abc2752@naver.com>
2023-04-20 12:06:20 +0900

    design: 답변 웹에디터 스타일 변경

--- client/src/components/Answer/CreateAnswer.jsx
+++ client/src/components/Answer/CreateAnswer.jsx
@@ -4,40 +4,40 @@ import { Editor } from '@toast-ui/react-editor';
 import { useRef } from 'react';
 import SharedButton from '../SharedButton';
 
+const Container = styled.div`
+  width: 727px;
+`;
+const YourAnswer = styled.div`
+  padding: 20px 0px;
+  font-size: 19px;
+  border-top: 1px solid hsl(210deg 8% 90%);
+`;
+const ButtonContainer = styled.div`
+  display: flex;
+  padding: 10px 0px 15px 0px;
+`;
+
+const EditorBox = styled(Editor)`
+  height: 255px;
+`;
+const EditorContainer = styled.div``;
+
 function CreateAnswer({ title, desc }) {
   const editorRef = useRef(null);
   const showEditorData = () => {
     console.log(editorRef.current?.getInstance().getHTML());
   };
 
-  const Container = styled.div`
-    width: 727px;
-  `;
-
-  const YourAnswer = styled.div`
-    padding: 20px 0px;
-    font-size: 19px;
-    border-top: 1px solid hsl(210deg 8% 90%);
-  `;
-
-  const ButtonContainer = styled.div`
-    display: flex;
-    padding: 10px 0px 15px 0px;
-  `;
-
-  const EditorBox = styled(Editor)`
-    height: 255px;
-  `;
-  const EditorContainer = styled(Editor)``;
-
   return (
     <Container>
-      <YourAnswer>Yor Answer</YourAnswer>
+      <YourAnswer>Your Answer</YourAnswer>
       <EditorContainer>
-        <EditorBox previewStyle='vertical' initialEditType='WYSIWYG' useCommandShortcut={true} ref={editorRef} onFocus={showEditorData} />
+        <label>{title}</label>
+        <p>{desc}</p>
+        <EditorBox previewStyle='vertical' initialEditType='wysiwyg' useCommandShortcut={true} ref={editorRef} onFocus={showEditorData} />
       </EditorContainer>
       <ButtonContainer>
-        <SharedButton buttonText='Post Your Answer' />{' '}
+        <SharedButton buttonText='Post Your Answer' />
       </ButtonContainer>
     </Container>
   );
