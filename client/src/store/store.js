d344f8e9394f5b1109354d1dcf76ed2b80a5611a
Choi-hyeona <xhrl523@naver.com>
2023-04-21 13:11:06 +0900

    feat: blacklist추가

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -2,13 +2,16 @@ import { configureStore } from '@reduxjs/toolkit';
 import storage from 'redux-persist/lib/storage';
 import { combineReducers } from 'redux';
 import { persistReducer } from 'redux-persist';
+import expireReducer from 'redux-persist-expire';
 
 import { userSlice } from '../reducer/userSlice';
+import { authSlice } from '../reducer/authSlice';
 import askSlice from '../reducer/askSlice';
 import questionSlice from '../reducer/questionSlice';
 
 const reducers = combineReducers({
   user: userSlice.reducer,
+  auth: authSlice.reducer,
   ask: askSlice.reducer,
   questions: questionSlice.reducer,
 });
@@ -16,7 +19,8 @@ const reducers = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
-  whitelist: ['user'],
+  whitelist: ['user', 'auth'],
+  blacklist: ['ask', 'questions'],
 };
 
 const persist = persistReducer(persistConfig, reducers);
