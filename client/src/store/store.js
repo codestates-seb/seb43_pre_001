faaab85df16aba63666220a31bf1aacea220b09f
Choi-hyeona <xhrl523@naver.com>
2023-04-21 12:53:29 +0900

    feat: 리덕스에 로컬스토리지 적용

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,14 +1,29 @@
 import { configureStore } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import { combineReducers } from 'redux';
+import { persistReducer } from 'redux-persist';
+
 import { userSlice } from '../reducer/userSlice';
 import askSlice from '../reducer/askSlice';
 import questionSlice from '../reducer/questionSlice';
 
+const reducers = combineReducers({
+  user: userSlice.reducer,
+  ask: askSlice.reducer,
+  questions: questionSlice.reducer,
+});
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user'],
+};
+
+const persist = persistReducer(persistConfig, reducers);
+
 const store = configureStore({
-  reducer: {
-    user: userSlice.reducer,
-    ask: askSlice.reducer,
-    questions: questionSlice.reducer,
-  },
+  reducer: persist,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
 
 export default store;
