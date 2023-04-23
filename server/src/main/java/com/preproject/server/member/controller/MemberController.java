a4f816a901be8c877a605cc0c8d70a7205691bfe
seunghyun711 <seunghyun711@gmail.com>
2023-04-23 15:29:41 +0900

    feat: 회원 정보 수정 구현

--- server/src/main/java/com/preproject/server/member/controller/MemberController.java
+++ server/src/main/java/com/preproject/server/member/controller/MemberController.java
@@ -1,30 +1,28 @@
 package com.preproject.server.member.controller;
 
+import com.preproject.server.auth.jwt.JwtTokenizer;
 import com.preproject.server.member.dto.MemberDto;
 import com.preproject.server.member.mapper.MemberMapper;
+import com.preproject.server.member.repository.MemberRepository;
 import com.preproject.server.member.service.MemberService;
 import com.preproject.server.member.entity.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
 @RestController
 @Validated
 @RequestMapping("/members")
+@RequiredArgsConstructor
 public class MemberController {
-    private MemberService memberService;
-    private MemberMapper mapper;
-
-    public MemberController(MemberService memberService, MemberMapper mapper) {
-        this.memberService = memberService;
-        this.mapper = mapper;
-    }
+    private final MemberService memberService;
+    private final MemberMapper mapper;
+    private final JwtTokenizer jwtTokenizer;
+    private final MemberRepository memberRepository;
 
     // 회원가입
     @PostMapping("/signup")
@@ -33,6 +31,17 @@ public class MemberController {
         Member createdMember = memberService.createMember(member);
 
         return new ResponseEntity<>(mapper.memberToMemberResponseDto(createdMember),HttpStatus.CREATED);
+    }
+
+    // 회원 정보 수정
+    @PatchMapping("/update/{member-id}")
+    public ResponseEntity patchMember(@PathVariable("member-id") long memberId,
+                                      @Valid @RequestBody MemberDto.Patch memberPatchDto) {
+        memberPatchDto.setMemberId(memberId);
+        Member member = mapper.memberPatchDtoToMember(memberPatchDto);
+        Member updatedMember = memberService.updateMember(member);
+
+        return new ResponseEntity<>(mapper.memberToMemberResponseDto(updatedMember), HttpStatus.OK);
     }
 
 }
--- server/src/main/java/com/preproject/server/member/service/MemberService.java
+++ server/src/main/java/com/preproject/server/member/service/MemberService.java
@@ -1,10 +1,15 @@
 package com.preproject.server.member.service;
 
+import com.preproject.server.auth.dto.LoginDto;
+import com.preproject.server.auth.jwt.JwtTokenizer;
 import com.preproject.server.auth.utils.MemberAuthorityUtils;
 import com.preproject.server.exception.BusinessLogicException;
 import com.preproject.server.exception.ExceptionCode;
 import com.preproject.server.member.entity.Member;
 import com.preproject.server.member.repository.MemberRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,13 +23,16 @@ public class MemberService {
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
     private final MemberAuthorityUtils authorityUtils;
+    private final JwtTokenizer jwtTokenizer;
 
     public MemberService(MemberRepository memberRepository,
                          PasswordEncoder passwordEncoder,
-                         MemberAuthorityUtils authorityUtils) {
+                         MemberAuthorityUtils authorityUtils,
+                         JwtTokenizer jwtTokenizer) {
         this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
         this.authorityUtils = authorityUtils;
+        this.jwtTokenizer = jwtTokenizer;
     }
 
     /*
@@ -50,10 +58,43 @@ public class MemberService {
         return memberRepository.save(member);
     }
 
+    /**
+     * <회원 정보 수정>
+     * 회원 정보는 닉네임, 비밀번호만 변경 가능
+     * 1. 수정
+     * 2. 저장
+     */
+    public Member updateMember(Member member) {
+        // 회원 검증
+        Member findMember = checkMember(member.getMemberId());
+        // 닉네임 수정
+        findMember.setNickname(member.getNickname());
+        // 비밀번호 수정 -> 암호화하여 저장
+        String updatedPassword = passwordEncoder.encode(findMember.getPassword());
+        findMember.setPassword(updatedPassword);
+
+        // 저장
+        return memberRepository.save(findMember);
+
+    }
+
     private void verifyExistsEmail(String email) throws Exception {
         Optional<Member> optionalMember = memberRepository.findByEmail(email);
         if (optionalMember.isPresent()) {
             throw new BusinessLogicException(ExceptionCode.EMAIL_EXISTS);
         }
     }
+
+    private void checkEmail(String email) {
+        Optional<Member> optionalMember = memberRepository.findByEmail(email);
+        if (!optionalMember.isPresent()) {
+            throw new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND);
+        }
+    }
+
+    private Member checkMember(long memberId) {
+        return memberRepository.findById(memberId).orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.MEMBER_NOT_FOUND));
+    }
+
 }
